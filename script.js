30fde4a7cc0f566b3d737f57b2ff1210f3670721
noceive <nocivescience@gmail.com>
2022-07-25 19:33:20 -0400

    terminado

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const showEl=document.getElementById('show');
+const hideEl=document.getElementById('hide');
 const addContainer=document.getElementById('add-container');
-const textos=['hola mundo', 'adios mundo'];
-function createCards(){
-  textos.forEach((data,index)=>{
+const textos=[
+  ['hola mundo', 'adios mundo','alugon dia seremos novios'],
+  ['como estas', 'estoy bien'],
+  ['amarillo','rojo']
+];
+function createCards(texto){
+  texto.forEach((data,index)=>{
     createCard(data,index)
   })
 }
@@ -11,12 +16,17 @@ function createCard(data,index){
   if(index===0){
     card.classList.add('active')
   }
-  card.innerHTML=`${data} y ${index}`
+  card.innerHTML=`${data} y ${index}`;
   addContainer.appendChild(card)
 }
-createCards();
+textos.map((texto)=>createCards(texto))
 showEl.addEventListener('click',()=>{
   setTimeout(()=>{
     addContainer.classList.add('show')
   },1000)
+})
+hideEl.addEventListener('click',()=>{
+  setTimeout(()=>{
+    addContainer.classList.remove('show')
+  })
 })
